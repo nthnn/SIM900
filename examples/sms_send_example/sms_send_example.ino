f5cf9d57202f3ebe295ffd95aee4a13197ab137a
nthnn <nathanneisip@gmail.com>
2023-10-15 02:23:21 +0800

    Better SMS example.

--- examples/sms_send_example/sms_send_example.ino
+++ examples/sms_send_example/sms_send_example.ino
@@ -1,11 +1,16 @@
 #include <SoftwareSerial.h>
 #include <sim900.h>
 
+SoftwareSerial shieldSerial(7, 8);
+
 void setup() {
   Serial.begin(9600);
-  SIM900 sim900(&SoftwareSerial(7, 8));
 
-  Serial.println(sim900.sendSMS("+XXxxxxxxxxxx", "Hello, world!"));
+  SIM900 sim900(&shieldSerial);
+  Serial.println(
+    sim900.sendSMS("+XXxxxxxxxxxx", "Hello, world!!")
+      ? "Sent!" : "Not sent."
+  );
   sim900.close();
 }
 
