d6d6a2e2e53b43be60aaa2713785d546e78a4b71
nthnn <nathanneisip@gmail.com>
2023-10-15 02:29:03 +0800

    Handshake to SIM900 shield example.

---
+++ examples/handshake/handshake.ino
@@ -0,0 +1,15 @@
+#include <SoftwareSerial.h>
+#include <sim900.h>
+
+SoftwareSerial shieldSerial(7, 8);
+
+void setup() {
+  Serial.begin(9600);
+  SIM900 sim900(&shieldSerial);
+
+  Serial.println(
+    sim900.handshake() ? "Handshaked!" : "Something went wrong."
+  );
+}
+
+void loop() { }
