f6f8a97beb0cbf0a814bd21c5a0cf615aae5ff69
nthnn <nathanneisip@gmail.com>
2023-10-17 23:07:31 +0800

    SIM900 definition for date and time.

--- src/sim900_defs.h
+++ src/sim900_defs.h
@@ -2,17 +2,23 @@
 #define SIM900_DEFS_H
 
 typedef enum _SIM900DialResult {
-  SIM900_DIAL_RESULT_NO_DIALTONE,
-  SIM900_DIAL_RESULT_BUSY,
-  SIM900_DIAL_RESULT_NO_CARRIER,
-  SIM900_DIAL_RESULT_NO_ANSWER,
-  SIM900_DIAL_RESULT_ERROR,
-  SIM900_DIAL_RESULT_OK
+    SIM900_DIAL_RESULT_NO_DIALTONE,
+    SIM900_DIAL_RESULT_BUSY,
+    SIM900_DIAL_RESULT_NO_CARRIER,
+    SIM900_DIAL_RESULT_NO_ANSWER,
+    SIM900_DIAL_RESULT_ERROR,
+    SIM900_DIAL_RESULT_OK
 } SIM900DialResult;
 
 typedef struct _SIM900Operator {
-  uint16_t status, format;
-  String name;
+    uint16_t status, format;
+    String name;
 } SIM900Operator;
+
+typedef struct _SIM900RTC {
+    uint8_t day, month, year,
+        hour, minute, second;
+    int8_t gmt;
+} SIM900RTC;
 
 #endif
